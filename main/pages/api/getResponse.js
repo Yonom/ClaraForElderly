e0651f969b23f9d09123434597a60d37530de6bb
Simon Farshid <Yonom@users.noreply.github.com>
2023-04-10 21:26:19 +0200

    Switch to gpt-3

--- main/pages/api/getResponse.js
+++ main/pages/api/getResponse.js
@@ -37,7 +37,7 @@ const handler = async (req) => {
       Accept: "text/event-stream",
     },
     body: JSON.stringify({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: [{ role: "system", content: initialPrompt(language) }, ...messages],
       temperature: 0.7,
       max_tokens: 1024,
