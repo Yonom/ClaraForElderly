4980021c7c9c1a638b02591f533f6cda6965f8d7
Elias Schwalme <132968734+eliasschwalme@users.noreply.github.com>
2023-08-02 22:07:54 +0100

    Update index.js

--- main/pages/index.js
+++ main/pages/index.js
@@ -141,6 +141,10 @@ export default function Home() {
               Hosted by{" "}
               <a href="https://www.linkedin.com/in/elias-schwalme-723b04205/" style={{ color: "white" }}>
                 Elias Schwalme
+              and Discord Server at{" "}
+              <a href="https://discord.gg/2MD7akWv" style={{ color: "white" }}>
+                Lee-AI -> Discord
+              <br />
               </a>
             </p>
           </div>
