9f52fc1b0824af65abe0c3a5b156fafb6e92b2f0
Simon Farshid <Yonom@users.noreply.github.com>
2023-11-15 21:10:47 +0000

    Use GPT-4 Turbo

--- main/pages/api/getResponse.js
+++ main/pages/api/getResponse.js
@@ -13,7 +13,7 @@ const handler = async (req) => {
       Accept: "text/event-stream",
     },
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4-1106-preview",
       messages,
       temperature: 0.7,
       max_tokens: 1024,
